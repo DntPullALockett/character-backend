bb15fbddd1dc15af2f4418c312faf01597b64a5c
Anthony Lockett <alockettjr@me.com>
2024-02-24 23:21:41 -0500

    added body parsign and colleciton endpoint

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
+type Collection struct {
+	Name string `json:"name" bson:"Name"`
+}
+
 func main() {
 	log.SetPrefix("character service: ")
 	log.SetFlags(0)
 
 	http.HandleFunc("POST /characters/create", createCharacterHandler)
+	http.HandleFunc("POST /collections/create", createCollectionHandler)
 
 	port := os.Getenv("PORT")
 
@@ -23,5 +29,17 @@ func main() {
 }
 
 func createCharacterHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("character created")
+
+}
+
+func createCollectionHandler(w http.ResponseWriter, r *http.Request) {
+	var collection Collection
+
+	err := json.NewDecoder(r.Body).Decode(&collection)
+
+	if err != nil {
+		fmt.Println("Error reading body")
+	}
+
+	fmt.Println(collection.Name)
 }
